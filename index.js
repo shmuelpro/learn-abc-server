f7761f9e9e07a93508893451c960a706f88dc0fc
shmuel PC <shmuelpro@gmail.com>
2020-01-19 14:48:47 +0200

    files and stuff

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+
+const bodyParser = require('body-parser')
+const app = express()
+app.use(bodyParser.json())
+const port = 4000
+app.use(express.static('public'))
+app.use(express.static('files'))
+app.get('/', (req, res) => res.send('Hello World!'))
+
+const questions = [
+    {type:"question",file:"/sounds/a.m4a",key:"a",image:"/images/question.png"},
+    {type:"letter",file:"/sounds/what.mp3",text:"A",image:"/images/question.png"},
+]
+
+app.get('/api/nextLetter',(req,res)=>{
+
+    
+    var index = getRandomInt(questions.length);    
+    res.json(questions[index])
+    
+
+})
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
