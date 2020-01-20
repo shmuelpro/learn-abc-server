059d769c9ca028c292b4832f8e7ce6f890555e6a
shmuel PC <shmuelpro@gmail.com>
2020-01-20 14:51:30 +0200

    a lot of changes yo

---
+++ public/precache-manifest.dd726a27d37e4ee534e83d8bcda0a6a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96ea98da8e143430315d5b6c03d6307",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba1450ec10cd7400e207",
+    "url": "/static/css/2.c4418c31.chunk.css"
+  },
+  {
+    "revision": "eff2ce07279afef558e9",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ba1450ec10cd7400e207",
+    "url": "/static/js/2.83e445cc.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.83e445cc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "eff2ce07279afef558e9",
+    "url": "/static/js/main.a47ab48c.chunk.js"
+  },
+  {
+    "revision": "95fd4d746a89ddd62e34",
+    "url": "/static/js/runtime-main.f06126d2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
